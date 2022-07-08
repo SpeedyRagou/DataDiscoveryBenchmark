3359b0f64b3ca43530c8bd0a759afaf8f307da29
Felix Neutatz <neutatz@gmail.com>
2022-07-08 13:37:12 +0200

    fix

--- datadiscoverybench/load_db.py
+++ datadiscoverybench/load_db.py
@@ -14,5 +14,5 @@ print('time to load the db: ' + str(time.time() - start))
 
 print(con.execute("SHOW TABLES;").fetchall())
 
-print(con.execute("SELECT COUNT(*) FROM AllTables WHERE CellValue='US';").fetchall())
+#print(con.execute("SELECT COUNT(*) FROM AllTables WHERE CellValue='US';").fetchall())
 print('show number of tables: ' + str(con.execute("SELECT COUNT(DISTINCT TableId) AS some_alias FROM AllTables;").fetchall()))
