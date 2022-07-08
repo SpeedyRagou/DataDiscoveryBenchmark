d21e5762d4a2c423859f0e18761f3e60c23a01a9
Felix Neutatz <neutatz@gmail.com>
2022-07-08 18:00:13 +0200

    fix

--- datadiscoverybench/load_db.py
+++ datadiscoverybench/load_db.py
@@ -1,5 +1,6 @@
 import duckdb
 import time
+import pickle
 # to start an in-memory database
 con = duckdb.connect(database=':memory:')
 
@@ -11,8 +12,18 @@ con.execute("IMPORT DATABASE '/home/felix/duckdb/dresden/db/';")
 
 print('time to load the db: ' + str(time.time() - start))
 
+path = '/home/mahdi/DWTC_json'
+my_path = '/home/felix/duckdb'
+
+#path = '/home/neutatz/Software/DataDiscoveryBenchmark/data/dresden'
+#my_path = '/home/neutatz/Software/DataDiscoveryBenchmark/data'
+
+data = pickle.load(open(my_path + '/dresden/import/dict.pickle', 'rb'))
 
 print(con.execute("SHOW TABLES;").fetchall())
 
+
+
 #print(con.execute("SELECT COUNT(*) FROM AllTables WHERE CellValue='US';").fetchall())
 print('show number of tables: ' + str(con.execute("SELECT COUNT(DISTINCT TableId) AS some_alias FROM AllTables;").fetchall()))
+print(len(data))
