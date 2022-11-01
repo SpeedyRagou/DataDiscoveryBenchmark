284f21bf675cf16b4a620edf4ab9387f40e9707e
Jannis Becktepe <jannis@becktepe.de>
2022-11-01 11:00:28 +0100

    fetch_candidates now receives DataFrame of examples

--- db_handler.py
+++ db_handler.py
@@ -1,4 +1,6 @@
 import duckdb
+import numpy as np
+
 from datadiscoverybench.utils import load_dresden_db
 from typing import List
 import pandas as pd
@@ -11,7 +13,7 @@ class DBHandler:
     Parameters
     ----------
     debug : bool
-        If true, the number of result rows is limited to 100.
+        If true, the number of result rows is limited to 10.
     """
     def __init__(self, debug: bool = True):
         self.con = duckdb.connect(database=':memory:')
@@ -19,7 +21,7 @@ class DBHandler:
 
         load_dresden_db(self.con)
 
-    def __generate_query(self, examples: List[List[str]], tau: int = 2) -> str:
+    def __generate_query(self, examples: np.ndarray, tau: int = 2) -> str:
         x_cols = len(examples) - 1
 
         # outer select
@@ -33,45 +35,46 @@ class DBHandler:
         for x in range(0, x_cols):
             joint_list = "','".join(examples[x])
 
-            query += f"(SELECT TableId, ColumnId " \
-                     f"FROM AllTables " \
-                     f"WHERE CellValue IN ('{joint_list}') " \
-                     f"GROUP BY TableId, ColumnId " \
-                     f"HAVING COUNT(DISTINCT CellValue) >= {tau}) AS colX{x + 1}, "
+            query += f"\n   (SELECT TableId, ColumnId " \
+                     f"\n   FROM AllTables " \
+                     f"\n   WHERE CellValue IN ('{joint_list}') " \
+                     f"\n   GROUP BY TableId, ColumnId " \
+                     f"\n   HAVING COUNT(DISTINCT CellValue) >= {tau}) AS colX{x + 1},\n"
 
         joint_list = "','".join(examples[x_cols])
-        query += f"(SELECT TableId, ColumnId " \
-                 f"FROM AllTables " \
-                 f"WHERE CellValue IN ('{joint_list}') " \
-                 f"GROUP BY TableId, ColumnId " \
-                 f"HAVING COUNT(DISTINCT CellValue) >= {tau}) AS colY "
+        query += f"\n   (SELECT TableId, ColumnId " \
+                 f"\n   FROM AllTables " \
+                 f"\n   WHERE CellValue IN ('{joint_list}') " \
+                 f"\n   GROUP BY TableId, ColumnId " \
+                 f"\n   HAVING COUNT(DISTINCT CellValue) >= {tau}) AS colY "
 
-        query += f"WHERE colX1.TableId = colY.TableId "
+        query += f"\nWHERE colX1.TableId = colY.TableId "
 
         for x in range(0, x_cols - 1):
-            query += f"AND colX{x + 1}.TableId = colX{x + 2}.TableId "
-            query += f"AND colX{x + 1}.ColumnId <> colX{x + 2}.ColumnId "
+            query += f"\nAND colX{x + 1}.TableId = colX{x + 2}.TableId "
+            query += f"\nAND colX{x + 1}.ColumnId <> colX{x + 2}.ColumnId "
 
         for x in range(0, x_cols):
-            query += f"AND colX{x + 1}.ColumnId <> colY.ColumnId "
+            query += f"\nAND colX{x + 1}.ColumnId <> colY.ColumnId "
 
         if self.debug:
-            query += "LIMIT 100"
+            query += "\nLIMIT 10;"
 
+        print(query)
         return query
 
-    def fetch_candidates(self, examples: List[List[str]], tau: int = 2) -> pd.DataFrame:
+    def fetch_candidates(self, examples: pd.DataFrame, tau: int = 2) -> pd.DataFrame:
         """
         Fetches all candidate column combinations that satisfy at least tau examples per column.
 
         Parameters
         ----------
-        examples : List[List[str]]
-            List of examples, e.g.
-            [[x11, x21, ...],
-             [x21, x22, ...],
-             ...
-             [y1, y2, ...]]
+        examples : pd.DataFrame
+            DataFrame of examples, e.g.
+
+                 0    1    2
+            0  x11  x21  y1
+            1  x21  x22  y2
 
         tau : int
             Minimum number of examples per column.
@@ -81,7 +84,8 @@ class DBHandler:
         pd.DataFrame
             Candidate tables/columns.
         """
-        return self.con.execute(self.__generate_query(examples, tau=tau)).fetch_df()
+        print(examples.to_numpy().T)
+        return self.con.execute(self.__generate_query(examples.to_numpy().T, tau=tau)).fetch_df()
 
     def fetch_table(self, table_id: int) -> pd.DataFrame:
         """
@@ -111,6 +115,8 @@ class DBHandler:
 
 if __name__ == "__main__":
     db = DBHandler()
-    db.fetch_table(1)
+    ex = pd.DataFrame([["x11", "x12", "y1 "], ["x21", "x22", "y2 "]])
+    db.fetch_candidates(ex)
+    #db.fetch_table(1)
 
 
