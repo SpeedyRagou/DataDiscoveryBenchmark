1b6a717d61480e1d1ca514be3edb0eb2bed178b4
SpeedyRagou <m.f.kuhlmann@gmail.com>
2022-11-15 11:43:39 +0100

    fixed a bug with testing script

--- dataxformer_group-c/testing_all_parquets.py
+++ dataxformer_group-c/testing_all_parquets.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     path_to_examples = Path("../dataxformer_group-c/Examples/benchmark/CountryToLanguage.csv").resolve()
     parquets = np.array([i for i in range(500)])
     parquets = parquets.reshape(10, 50)
-    for i in parquets.shape[0]:
+    for i in range(parquets.shape[0]):
 
         print(f"\nParquet Package {i}\n")
 
