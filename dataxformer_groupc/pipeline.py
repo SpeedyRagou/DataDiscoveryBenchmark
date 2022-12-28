291b2d29e2232ca473df5d4aa4a291b369495cbc
SHA-T <tel.shamoyan@gmail.com>
2022-12-28 20:58:48 +0100

    Replaced F1-Score by Precision and Recall (But there is a bug with the number of results)

--- dataxformer_groupc/pipeline.py
+++ dataxformer_groupc/pipeline.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 from pathlib import Path
 
 import pandas as pd
-# from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score, recall_score
 from DataXFormer import DataXFormer
 
 
@@ -24,17 +24,18 @@ def create_examples_csv(path):
     # Resolve path, store filename, create dataframe
     path_resolved = Path(path).resolve()
     filename = Path(path_resolved).stem
-    df = pd.read_csv(path_resolved, dtype=str, encoding="ISO-8859-1")
+    benchmark_df = pd.read_csv(path_resolved, dtype=str, encoding="ISO-8859-1")
 
     # Cap the size of the csv to 20 rows and save
-    df = df.head(20)
-    df.to_csv("./data/benchmark/{0}.csv".format(filename), index=False)
+    benchmark_df = benchmark_df.head(20)
+    benchmark_df.to_csv("./data/benchmark/{0}.csv".format(filename), index=False)
 
     # Keep the first 5 transformations as examples and delete the y-values of the rest
-    df.iloc[:, -1] = df.iloc[:5, -1]
-    df.to_csv("./data/examples/{0}.csv".format(filename), index=False)
+    examples_df = benchmark_df.copy(deep=True)
+    examples_df.iloc[:, -1] = examples_df.iloc[:5, -1]
+    examples_df.to_csv("./data/examples/{0}.csv".format(filename), index=False)
 
-    return df
+    return benchmark_df, examples_df
 
 
 if __name__ == "__main__":
@@ -43,22 +44,26 @@ if __name__ == "__main__":
 
     files = [f for f in listdir(path_benchmark) if isfile(join(path_benchmark, f))]
 
-    f1 = 0
+    prec = 0
+    recall = 0
 
     for f in files:
         p = Path(path_benchmark + f).resolve()
-        df = create_examples_csv(p)
+        benchmark_df, examples_df = create_examples_csv(p)
 
-        dataxformer = DataXFormer(verbose=True, use_table_joiner=False, debug=False,
-                                  db_file_path=Path("/home/groupc/gittables_DXF_all.duckdb"))
-        transformed_df = dataxformer.run(df)
+        dataxformer = DataXFormer(verbose=True, use_table_joiner=False, debug=False)#,
+                                  #db_file_path=Path("/home/groupc/gittables_DXF_all.duckdb"))
+        transformed_df = dataxformer.run(examples_df)
 
-        # DataXFormer has to return all examples and query values for the following to work
-        """
-        f1 = f1_score(df.iloc[:, -1].to_numpy(), transformed_df.iloc[:, -2].to_numpy(), average='micro')
-        """
-        # Until then ...
-        f1 += 0.85
+        # Precision and Recall
+        prec += precision_score(benchmark_df.iloc[:, -1].astype(str).to_numpy(),
+                                transformed_df.iloc[:, -1].astype(str).to_numpy(),
+                                average=None, zero_division=0)
+        recall += recall_score(benchmark_df.iloc[:, -1].astype(str).to_numpy(),
+                               transformed_df.iloc[:, -1].astype(str).to_numpy(),
+                               average=None, zero_division=0)
 
-    f1 /= len(files)
-    print("Average F1-Score:", f1)
+    prec /= len(files)
+    recall /= len(files)
+    print("Average Precision:", prec)
+    print("Average Recall:", recall)
