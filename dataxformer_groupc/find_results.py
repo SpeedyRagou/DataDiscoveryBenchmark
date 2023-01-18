3369499b21464581623863ce46553f8954e04717
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-01-18 18:16:21 +0100

    result checking

--- dataxformer_groupc/find_results.py
+++ dataxformer_groupc/find_results.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 from pathlib import Path
 
 import pandas as pd
+from sklearn.metrics import precision_recall_fscore_support
 
 path_to_examples = Path("./data/examples").resolve()
 
@@ -24,28 +25,43 @@ for f in files:
 
 print(pairs)
 
+precision = 0
+recall = 0
+f1 = 0
+length = 0
+
 for pair in pairs:
     path_to_examples_absolute = path_to_examples.joinpath(pair[0]).resolve()
     path_to_results_absolute = path_to_results.joinpath(pair[1]).resolve()
+    path_to_benchmark_absolute = path_to_benchmark.joinpath(pair[0]).resolve()
     try:
         res_df = pd.read_csv(path_to_results_absolute, dtype=str, encoding="ISO-8859-1")
         ex_df = pd.read_csv(path_to_examples_absolute, dtype=str, encoding="ISO-8859-1")
+        bench_df = pd.read_csv(path_to_benchmark_absolute, dtype=str, encoding="ISO-8859-1")
     except Exception:
         continue
 
-    if res_df.shape[0] == ex_df.shape[0]:
-        path_to_benchmark_absolute = path_to_benchmark.joinpath(pair[0]).resolve()
-        bench_df = pd.read_csv(path_to_benchmark_absolute, dtype=str, encoding="ISO-8859-1")
+    res_df = res_df.drop_duplicates.drop_duplicates(subset=res_df.columns[:res_df.shape[1] - 1])
+    bench_df = bench_df.astype(str).applymap(str.lower)
 
-        ex_df = ex_df.dropna()
-        res_df_r = res_df.dropna()
+    ex_df = ex_df.dropna()
+    res_df_r = res_df.dropna()
+    print(f"------ For pair {pair} ---------")
+    print(f"{ex_df.shape[0]} : {res_df_r.shape[0]}")
+    if ex_df.shape[0] < res_df_r.shape[0]:
+        length += 1
         print(f"------ For pair {pair} ---------")
         print(f"{ex_df.shape[0]} : {res_df_r.shape[0]}")
-        if ex_df.shape[0] < res_df_r.shape[0]:
-            print(f"------ For pair {pair} ---------")
-            print(f"{ex_df.shape[0]} : {res_df_r.shape[0]}")
-            res_df.sort_values(res_df.columns[0], axis=0, inplace=True)
-            print(res_df)
-            bench_df.sort_values(bench_df.columns[0], axis=0, inplace=True)
-            print(bench_df)
+        res_df.sort_values(res_df.columns[:res_df.shape[1] - 1], axis=0, inplace=True)
 
+        bench_df.sort_values(bench_df.columns[:res_df.shape[1] - 1], axis=0, inplace=True)
+
+        precision_file, recall_file, f1_file, support = precision_recall_fscore_support(bench_df.iloc[:, -1].to_numpy().astype(str), res_df.iloc[:, -1].to_numpy().astype(str), average='micro')
+
+        print(f"Precision: {precision_file}, Recall: {recall_file}, F1: {f1_file}")
+
+        precision += precision_file
+        recall += recall_file
+        f1 += f1_file
+
+    print(f"Avg. Precision: {precision/length}, Avg. Recall: {recall/length}, Avg. F1: {f1/length}")
