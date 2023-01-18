fccab0f90a9e658e53aaa3bf68295a9c1ce7f015
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-01-18 12:36:05 +0100

    added result discovery

--- dataxformer_groupc/find_results.py
+++ dataxformer_groupc/find_results.py
@@ -7,10 +7,11 @@ import pandas as pd
 path_to_examples = Path("./data/examples").resolve()
 
 path_to_results = Path("./data/results").resolve()
+path_to_benchmark = Path("./data/benchmark").resolve()
 
 path_examples = "data/examples/"
 path_results = "data/results/"
-
+path_to_bench = "data/benchmark/"
 files = [f for f in listdir(path_examples) if isfile(join(path_examples, f))]
 files_res = [f for f in listdir(path_results) if isfile(join(path_results, f))]
 pairs = []
@@ -32,15 +33,17 @@ for pair in pairs:
     except Exception:
         continue
 
-
-
     if res_df.shape[0] == ex_df.shape[0]:
+        path_to_benchmark_absolute = path_to_benchmark.joinpath(pair[0]).resolve()
+        bench_df = pd.read_csv(path_to_benchmark_absolute, dtype=str, encoding="ISO-8859-1")
+
         ex_df = ex_df.dropna()
-        res_df = res_df.dropna()
+        res_df_r = res_df.dropna()
 
         print(f"------ For pair {pair} ---------")
-        print(f"{ex_df.shape[0]} : {res_df.shape[0]}")
-        print(ex_df)
+        print(f"{ex_df.shape[0]} : {res_df_r.shape[0]}")
+        res_df.sort_values(res_df.columns[0], axis=0, inplace=True)
         print(res_df)
-
+        bench_df.sort_values(bench_df.columns[0], axis=0, inplace=True)
+        print(bench_df)
 
