8cfd843e19e04022aeff550f6b2b3d97325b3502
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-01-18 12:09:11 +0100

    added result discovery

--- dataxformer_groupc/find_results.py
+++ dataxformer_groupc/find_results.py
@@ -11,5 +11,10 @@ path_results = "data/results/"
 
 files = [f for f in listdir(path_examples) if isfile(join(path_examples, f))]
 files_res = [f for f in listdir(path_results) if isfile(join(path_results, f))]
+pairs = []
 
-print(list(zip(files, files_res)))
+for f in files:
+    for f_res in files_res:
+        if f[:len(f) - 4] in f_res:
+            pairs.append((f, f_res))
+            break
